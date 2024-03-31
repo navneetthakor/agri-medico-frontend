76eb78f90755cbe903e711ee3f5ced2b82db9ba5
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-03-31 12:20:55 +0530

    mui + router setup completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 
+// material ui setup 
+import {createTheme} from "@mui/material/styles"
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+import { ThemeProvider } from "@emotion/react";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Sample from "./components/Sample";
+import { CssBaseline } from "@mui/material";
+
+
+// router setup 
+
+
 
 function App() {
+  // setting up theme 
+  const mode = useSelector((state) => state.currMode.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode]);
+
+  // settingup the router 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route exact path="/" element={<Sample/>}>
+
+    </Route>
+  ))
   return (
     <>
-    <div>agri-medico-frontend</div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
     </>
   );
 }
---
+++ src/components/Sample.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function Sample() {
+  return (
+    <Box>
+      Hello
+    </Box>
+  )
+}
+
+export default Sample
