5079f6404e567238e178136e238f29e0a576b677
Rohanshu-33 <banodharohanshu@gmail.com>
2024-04-01 22:41:12 +0530

    welcome page css modification and fetch done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,10 @@
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../asset/Agrilogo.png'
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Alert from '@mui/material/Alert';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -15,10 +16,24 @@ const VisuallyHiddenInput = styled('input')({
     left: 0,
     whiteSpace: 'nowrap',
     width: 1,
-  });
+});
 
-  
+
 const Welcome = () => {
+
+
+    const handleImageSubmit = async (event) => {
+        const file = event.target.files[0]
+        const formData = new FormData();
+        formData.append('image', file);
+        const response = await fetch(`http://localhost:5001/flask/fetchdiseasename`, {
+            method: "POST",
+            mode: "cors",
+            body: formData
+        });
+        const json = await response.json();
+        console.log(json);
+    }
 
 
     return (
@@ -29,22 +44,32 @@ const Welcome = () => {
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center'
+            }}>
+                <img src={logo} height={'100px'} width={"100px"} alt="Logo" style={{marginTop:'-10vh'}} />
+                <h1 style={{ paddingTop: '10px', fontSize:'52px', color:'transparent', background: 'linear-gradient(45deg, #4285f4, #9b72cb, #9b72cb, #d96570, #131314)', backgroundClip: 'text' }}>Welcome to Agri-Medico,</h1>
+                <h2 style={{ marginTop:'-6vh', marginLeft: '-17vw', fontSize:'48px', color:'#7a7f9d'}}>Your AI Doctor...</h2>
+            </Box>
+            <Box sx={{
+                padding: '100px 0px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '-7vh'
 
             }}>
-                <img src={logo} height={'200px'} width={"200px"} alt="" />
-                <h1 style={{ paddingTop: '10px' }}>Welcome to Agri-Medico, Your AI Doctor...</h1>
-
                 <Button
                     component="label"
-                    role={undefined}
                     variant="contained"
-                    tabIndex={-1}
+                    tabIndex={0}
                     startIcon={<CloudUploadIcon />}
-                    sx={{marginTop:'70px'}}
+                    type="submit"
+                    onChange={handleImageSubmit}
                 >
                     Upload file
                     <VisuallyHiddenInput type="file" />
                 </Button>
+
             </Box>
         </div>
     )
